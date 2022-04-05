49b4f4b12b88c104f3e82f8f3a48291e8d6ada3c
Soham Sarkar <soham.ss1@gmail.com>
2022-04-05 11:56:52 +0530

    vscode folder + nextui provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,19 @@ import * as ReactDOMClient from 'react-dom/client'
 import './index.css'
 import App from './App'
 import {BrowserRouter} from 'react-router-dom'
+import {NextUIProvider} from '@nextui-org/react'
 
 function Root() {
   return (
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <NextUIProvider>
+          <App />
+        </NextUIProvider>
       </BrowserRouter>
     </React.StrictMode>
   )
 }
 const root = ReactDOMClient.createRoot(document.getElementById('root')!)
+
 root.render(<Root />)
