40dd4f6d3bad37856e47d04943f157707a9c4401
Soham Sarkar <soham.ss1@gmail.com>
2022-04-05 10:51:19 +0530

    react 18 + pnpm fix + node version

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import * as React from 'react'
+import * as ReactDOMClient from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+function Root() {
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+}
+const root = ReactDOMClient.createRoot(document.getElementById('root')!)
+root.render(<Root />)
