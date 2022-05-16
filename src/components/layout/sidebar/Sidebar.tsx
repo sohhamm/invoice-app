b2907fe2bd35cee72101430d70e39473846a56bc
Soham Sarkar <soham.ss1@gmail.com>
2022-05-16 13:35:21 +0530

    feat: sidebar ui

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,7 +1,22 @@
 import * as React from 'react'
 import styles from './sidebar.module.css'
-import {Container} from '@nextui-org/react'
+import logo from '@/assets/logo.svg'
+import {Container, Image} from '@nextui-org/react'
 
 export default function Sidebar() {
-  return <Container className={styles.box}>Sidebar</Container>
+  return (
+    <Container className={styles.box}>
+      <Container className={styles.topBox}>
+        <Container className={styles.topBoxBottom} />
+        <Image
+          src={logo}
+          alt="invoice logo"
+          className={styles.logo}
+          width={'40px'}
+          height={'45px'}
+        />
+      </Container>
+      Sidebar
+    </Container>
+  )
 }
